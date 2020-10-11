2ed07c86b05ebf5781a310dade7c4bd46c30cde2
komildeveloper <64638545+komildeveloper@users.noreply.github.com>
2020-10-11 15:54:44 +0500

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization: 'Client-ID S1-sS5OmmLMFZf9JbCqoPzig_eJ58S2I_IuSwhyCX48'
+    Authorization: 'Client-ID Your_Client_ID'
   }
 });
